b368b349eaa730a107f5b9b4374d834c9e5225a2
HiYong <maple77582@gmail.com>
2026-01-05 16:16:05 +0900

    [level 0] Title: 덧셈식 출력하기, Time: 17.57 ms, Memory: 16.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Diagnostics.CodeAnalysis;  
+
+public class Example
+{
+    public static void Main()
+    {
+        String[] s;
+
+        Console.Clear();
+        s = Console.ReadLine().Split(' ');
+
+        int a = Int32.Parse(s[0]);
+        int b = Int32.Parse(s[1]);
+        
+
+        Console.Write($"{a} + {b} = "); Console.Write($"{a + b}");
+    }
+}
